73ed58d3bb9ffb77dc5dc07efac8c383c2435836
BoxEdition <2111221@my.ipleiria.pt>
2019-06-11 16:14:27 +0100

    Add Post Class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post (models.Model):
+    title = models.CharField(max_length=255)
+    content= models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # Author -> on_delete will make sure that in the case of a USER gets DELETED,
+    # the posts associated with the USER WILL also be DELETED
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
