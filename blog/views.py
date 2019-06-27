01192daffb2311d481d271a92053faf0bceb5d68
BoxEdition <2111221@my.ipleiria.pt>
2019-06-27 20:43:22 +0100

    Startapp for Registration

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,7 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
