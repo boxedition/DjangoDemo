b8c853cfedc1a3b3150416604c8224957167ca25
BoxEdition <2111221@my.ipleiria.pt>
2019-07-28 09:59:54 +0100

    Migration of Profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to=user_directory_path)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
